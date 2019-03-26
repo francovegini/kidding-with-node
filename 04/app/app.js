b778c991c8bb65a3f30845493411980cd9927a26
Franco Vegini <francovegini@hotmail.com>
2019-03-25 22:01:16 -0300

    ajust

--- 04/app/app.js
+++ 04/app/app.js
@@ -13,18 +13,17 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'njk');
-var njk = expressNunjucks(app,{watch:true, noCache: true});
+var njk = expressNunjucks(app, {watch: true, noCache: true});
 
 app.use(session({
-  secret: 'teste sessoes',
-  resave: false,
-  saveUninitialized: false,
-
+    secret: 'teste sessoes',
+    resave: false,
+    saveUninitialized: false,
 }));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,21 +31,21 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
